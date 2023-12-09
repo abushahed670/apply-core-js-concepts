ba6a0cdc76191e3c3a242ab679e7577baf5072f2
abushahed670 <abu.shahed670@gmail.com>
2023-12-10 00:46:09 +0600

    var let const

---
+++ recap.js
@@ -0,0 +1,13 @@
+ //previous class discuss
+ /*
+ 1. conditional
+ 2. loop
+ 3. variable dec.
+ 4. function dec.
+ 5. parameter 
+ 6. return
+ 7. continue
+ 8. break
+ 9. object dec
+ 10. object vs parameter
+  */
---
+++ var-let-const.js
@@ -0,0 +1,6 @@
+let age =11;
+age = 12;
+const countryName = 'Bangladesh';
+// countryName = 'nepal' 
+// seta kora jabe na bks const er variable change hoi na
+console.log(age, countryName);
